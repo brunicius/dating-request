fa4e04332d2a51f8bb061387ce8bf545a7d93416
Bruno Vinicius <57375055+brunicius@users.noreply.github.com>
2023-05-04 00:08:48 -0300

    🚀 perf(App.tsx): add Main component to render 🎨 style(App.tsx): remove unused import statement The unused import statement for the Button component from the Material UI library has been removed to improve code readability. The Main component has been added to the render method to display the main content of the application.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@mui/material"
 import Header from "./components/Header"
+import Main from "./components/Main"
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
     </>
   )
 }
