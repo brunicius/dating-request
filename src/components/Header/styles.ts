cf1a16ab3b4fb501519309beb8b30b13ded3c24e
Bruno Vinicius <57375055+brunicius@users.noreply.github.com>
2023-05-09 13:20:15 -0300

    🎨 style(Header/styles.ts): add RGB animation to Icon and Typography components The RGB animation is added to the Icon and Typography components to make the header more visually appealing. The animation cycles through different colors in a smooth and continuous manner.

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,4 +1,4 @@
-import { Box, styled } from "@mui/material"
+import { Box, Icon, Typography, keyframes, styled } from "@mui/material"
 
 export const Container = styled(Box)(({ theme }) => ({
   padding: theme.spacing(4),
@@ -6,4 +6,38 @@ export const Container = styled(Box)(({ theme }) => ({
   // fontSize: "2px",
   // background: "blue",
 }))
+
+export const RgbIcon = styled(Icon)(() => ({
+  animationName: rgbKeyframes,
+  animationDuration: `20s`,
+  animationIterationCount: "infinite",
+  animationTimingFunction: "ease-in-out",
+  fontSize: 64,
+  // transition: "all ease-in-out 1s",
+}))
+
+export const RgbText = styled(Typography)(() => ({
+  animationName: rgbKeyframes,
+  animationDuration: `20s`,
+  animationIterationCount: "infinite",
+  animationTimingFunction: "ease-in-out",
+}))
+
+const rgbKeyframes = keyframes`
+  0% {
+    color: #69b7eb;
+  }
+  25% {
+    color: #b3dbd3;
+  }
+  50% {
+    color: #f4d6db;
+  }
+  75% {
+    color: #ddffd9;
+  }
+  100% {
+    color: #69b7eb
+  }
+`
 
